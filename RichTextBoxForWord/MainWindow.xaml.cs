1f5bba4cf471c60345d00f296b09dfbebb7f6405
Dzmitry Salodki <dsalodki@gmail.com>
2022-11-07 18:28:33 +0300

    save rtf

--- RichTextBoxForWord/MainWindow.xaml.cs
+++ RichTextBoxForWord/MainWindow.xaml.cs
@@ -5,8 +5,11 @@ using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
+using System.Windows.Shapes;
+using System.Xml.Linq;
 using static System.Net.Mime.MediaTypeNames;
 using Application = Microsoft.Office.Interop.Word.Application;
+using Document = Microsoft.Office.Interop.Word.Document;
 
 namespace RichTextBoxForWord
 {
@@ -31,57 +34,32 @@ namespace RichTextBoxForWord
 
                 if (ofd.CheckFileExists && new[] { ".docx", ".doc", ".txt", ".rtf" }.Contains(System.IO.Path.GetExtension(originalfilename).ToLower()))
                 {
-                    Microsoft.Office.Interop.Word.Application wordObject = new Microsoft.Office.Interop.Word.Application();
+                    Application wordObject = new Application();
                     object File = originalfilename;
                     object nullobject = System.Reflection.Missing.Value;
-                    Microsoft.Office.Interop.Word.Application wordobject = new Microsoft.Office.Interop.Word.Application();
+                    Application wordobject = new Application();
                     wordobject.DisplayAlerts = Microsoft.Office.Interop.Word.WdAlertLevel.wdAlertsNone;
-                    Microsoft.Office.Interop.Word._Document docs = wordObject.Documents.Open(ref File, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject);
+                    Document docs = wordObject.Documents.Open(ref File, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject);
                     docs.ActiveWindow.Selection.WholeStory();
                     docs.ActiveWindow.Selection.Copy();
-                    rtfMain.Paste();
+
+                    MemoryStream stream = new MemoryStream(Encoding.Default.GetBytes(Clipboard.GetText(TextDataFormat.Rtf)));
+                    rtfMain.Selection.Load(stream, DataFormats.Rtf);
+
                     docs.Close(ref nullobject, ref nullobject, ref nullobject);
                     wordobject.Quit(ref nullobject, ref nullobject, ref nullobject);
-
-
-                    MessageBox.Show("file loaded");
                 }
             }
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            Application application = new Application();
-            object File = "D:\\DOCs\\testRu.docx"; //this is the path
-            object nullobject = System.Reflection.Missing.Value;
-            application.DisplayAlerts = Microsoft.Office.Interop.Word.WdAlertLevel.wdAlertsNone; 
-            Microsoft.Office.Interop.Word._Document docs =
-            application.Documents.Open(ref File, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject); 
-            docs.ActiveWindow.Selection.WholeStory();
-            docs.ActiveWindow.Selection.Copy();
-
-
-            MemoryStream stream = new MemoryStream(ASCIIEncoding.Default.GetBytes(Clipboard.GetText(TextDataFormat.Rtf)));
-            rtfMain.Selection.Load(stream, DataFormats.Rtf);
-
-            //TextRange tr = new TextRange(rtfMain.Document.ContentStart, rtfMain.Document.ContentEnd);
-
-            //var bytes = UTF8Encoding.UTF8.GetBytes(Clipboard.GetText(TextDataFormat.Rtf));
-            //MemoryStream ms = new MemoryStream(bytes);
-            //tr.Save(ms, DataFormats.Rtf);
-            //string xamlText = UTF8Encoding.UTF8.GetString(ms.ToArray());
-
-            //Clipboard.SetText(docs.ActiveWindow.Selection.Text, TextDataFormat.Rtf);
-
-            //rtfMain.Paste();
-
-            //rtfMain.AppendText(Clipboard.GetText(TextDataFormat.Rtf));
-
-            //rtfMain.Document.Blocks.Clear();
-            //rtfMain.Document.Blocks.Add(new Paragraph(new Run(Clipboard.GetText(TextDataFormat.Rtf))));
-
-            docs.Close(ref nullobject, ref nullobject, ref nullobject);
-            application.Quit(ref nullobject, ref nullobject, ref nullobject);
+            var fileName = "result.rtf";
+            using(var fs = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+            {
+                var rtbText = new TextRange(rtfMain.Document.ContentStart, rtfMain.Document.ContentEnd);
+                rtbText.Save(fs, DataFormats.Rtf);
+            }
         }
     }
 }
